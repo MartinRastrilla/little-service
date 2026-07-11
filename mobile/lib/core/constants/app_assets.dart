562cab5de3b7ab197f7f76a244a8a6d3bdc20091
MartinRastrilla <113646546+MartinRastrilla@users.noreply.github.com>
2026-07-11 13:05:57 -0300

    style: assets config

---
+++ mobile/lib/core/constants/app_assets.dart
@@ -0,0 +1,7 @@
+abstract final class AppAssets {
+  static const _brand = 'assets/images/brand';
+  static const _illustrations = 'assets/images/illustrations';
+
+  static const littleServiceIsotipo = '$_brand/little-service-isotipo.png';
+  static const landscape = '$_illustrations/landscape-asset.png';
+}
